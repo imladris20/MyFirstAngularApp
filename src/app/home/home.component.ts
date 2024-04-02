3e72a3069da9fa2c2746d0075d9e5e16d2c396bd
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-02 18:21:37 +0800

    Add housing-location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousingLocationComponent],
   // templateUrl: './home.component.html',
   template: `
     <section>
@@ -12,7 +13,9 @@ import { Component } from '@angular/core';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
-    <section class="results"></section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `,
   styleUrl: './home.component.css',
 })
