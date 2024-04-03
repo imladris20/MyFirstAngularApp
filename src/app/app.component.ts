74c732313ebeba837a2749fa7fb71ccd98f3c713
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-03 10:59:22 +0800

    Make Home Component as default route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   // templateUrl: './app.component.html',
   template: `
     <main>
@@ -18,7 +18,8 @@ import { HomeComponent } from './home/home.component';
         />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <!-- <app-home></app-home> -->
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
