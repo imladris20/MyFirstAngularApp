432a65f0bad092e9d3fcaa079b116ba8d1608c79
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-03 15:52:49 +0800

    Make logo able to click to back to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,14 @@ import { HomeComponent } from './home/home.component';
   template: `
     <main>
       <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          aria-hidden="true"
-        />
+        <a href="/">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </a>
       </header>
       <section class="content">
         <!-- <app-home></app-home> -->
